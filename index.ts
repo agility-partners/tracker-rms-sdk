b29f90906ba690b39d7440d2fc270c80d8408996
Andy Warns <andy.warns@impower.ai>
2024-10-27 22:31:29 -0400

    asfd

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
-// Export the main Tracker class
 export { default as Tracker } from './src/tracker';
-
-// Export additional API modules
-export { Contacts } from './src/api/contacts'; // Only if you expect users to access Contacts directly
+export * from './src/types'
